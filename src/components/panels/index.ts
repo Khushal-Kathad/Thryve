90a5b3dc387c6cbc6496e5c49f407e2226350f77
Khushal Kathad <khushal.kathad@gmail.com>
2026-01-13 19:35:55 +0530

    chore: Clean main branch - keep only README
    Development code is in dev branch
    Deployment code is in deployment branch

--- src/components/panels/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as ThreadsPanel } from './ThreadsPanel';
-export { default as MentionsPanel } from './MentionsPanel';
-export { default as SavedPanel } from './SavedPanel';
-export { default as PeoplePanel } from './PeoplePanel';
-export { default as SettingsPanel } from './SettingsPanel';
